4c07c3a280cd009a42e64bd4efcb57c3a2b1062a
ArtiomB5 <artiomb5@yandex.by>
2022-09-09 22:17:03 +0600

    l047

---
+++ part_5_classes/l_047_this_practice.ts
@@ -0,0 +1,44 @@
+class Payment047 {
+    private date: Date = new Date();
+
+    getDate() {
+        return this.date;
+    }
+
+    getDateArrow = () => {
+        return this.date;
+    }
+}
+
+const p = new Payment047();
+
+const user047 = {
+    id: 1,
+
+    //тк метод getDate сформирован с использованием this то 
+    //this, формирующий getDate в строке 13, ссылается на сам объект user047 - контекст класса потерян
+    //это можно исправить с помощью bind - передав туда объект, контекст которого следует использовать
+    paymentDate: p.getDate.bind(p),
+
+    //в данном случе не испольхзуется bind тк при реализации методов через стредочные функции
+    //контекст класса всегда привязан к методу, реализованному таким образом - см троку 8
+    paymentDateArrow: p.getDateArrow,
+}
+
+console.log(p.getDate());
+console.log(user047.paymentDate());
+console.log(user047.paymentDateArrow());
+
+class PaymentPersistent extends Payment047 {
+    save1() {
+        return super.getDate();
+        //return super.getDateArrow() работать не будет
+    }
+
+    save2() {
+        return this.getDateArrow();
+    }
+}
+
+console.log(new PaymentPersistent().save1())
+console.log(new PaymentPersistent().save2())
