9c45d0091544003e576c51e9ce02c7ec44b07a06
ArtiomB5 <artiomb5@yandex.by>
2022-09-12 22:22:34 +0600

    l061

---
+++ part_7_generics/l_061_string_conversion_fucntion.ts
@@ -0,0 +1,24 @@
+function toString<T>(param:T): string | undefined{
+    if (Array.isArray(param)) {
+        return param.toString()
+    }
+    switch (typeof param) {
+        case 'string':
+            return param;
+        case 'number':
+        case 'symbol':
+        case 'bigint':
+        case 'boolean':
+        case 'function':
+            return param.toString();
+        case "object":
+            return JSON.stringify(param)
+        default:
+            return undefined;    
+    }
+}
+
+console.log(typeof toString(1));
+console.log(typeof toString(true));
+console.log(typeof toString(['a', 'b']));
+console.log(typeof toString({a: 1}));
