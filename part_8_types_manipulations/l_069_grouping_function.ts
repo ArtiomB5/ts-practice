8383594529ef90ecb4ae0021a573e4f53e2ebd55
ArtiomB5 <artiomb5@yandex.by>
2022-09-18 19:39:08 +0600

    l069

--- part_8_types_manipulations/l_069_grouping_function.ts
+++ part_8_types_manipulations/l_069_grouping_function.ts
@@ -1,32 +1,48 @@
 interface Data069 {
-    group: number;
-    name: string;
+  group: number;
+  name: string;
 }
 
 const data069: Data069[] = [
-    {group: 1, name: 'a'},
-    {group: 1, name: 'b'},
-    {group: 2, name: 'c'}
-]
+  { group: 1, name: "a" },
+  { group: 1, name: "b" },
+  { group: 2, name: "c" },
+];
 
-function group069 <T, K extends keyof T>(data: T[], groupingBy: K) {
-    const sortingParamsArray = new Set(data.map((i: T) => i[groupingBy]));
-    const sortedData: {[key: string]: T[]} = {}
-    for (let key of sortingParamsArray) {
-        sortedData[key as keyof typeof sortedData] = []
-    }
-    data.forEach(i => sortedData[String(i[groupingBy])].push(i))
-    return sortedData
+function group069<T, K extends keyof T>(data: T[], groupingBy: K) {
+  const sortingParamsArray = new Set(data.map((i: T) => i[groupingBy]));
+  const sortedData: { [key: string]: T[] } = {};
+  for (let key of sortingParamsArray) {
+    sortedData[key as keyof typeof sortedData] = [];
+  }
+  data.forEach((i) => sortedData[String(i[groupingBy])].push(i));
+  return sortedData;
 }
 
-console.log(group069(data069, 'group'));
+console.log(group069<Data069, keyof Data069>(data069, "group"));
 
 interface IGroup<T> {
-    [key: string]:T[] 
+  [key: string]: T[];
 }
 
 type KeyType069 = string | number | symbol;
 
-function group069a <T extends Record<KeyType069, any>>(array: T[], key: keyof T): IGroup<T> {
-    return {}
+function group069a<T extends Record<KeyType069, any>>(
+  array: T[],
+  key: keyof T
+): IGroup<T> {
+  return array.reduce<IGroup<T>>((map: IGroup<T>, item) => {
+    const itemKey = item[key];
+    let curEl = map[itemKey];
+    if (Array.isArray(curEl)) {
+      curEl.push(item);
+    } else {
+      curEl = [item];
+    }
+    map[itemKey] = curEl;
+    return map;
+  }, {});
 }
+
+const resGroup = group069a<Data069>(data069, 'group');
+console.log(resGroup);
